27e11e1dfdf4d21c0d785d781ecf9d05b4aae5d0
Salvatore-git <salvitomendi@gmail.com>
2026-05-11 21:32:52 -0500

    Update ejemplo3.cpp

--- Corte 2/Teoria /Punteros/C++/ejemplo3.cpp
+++ Corte 2/Teoria /Punteros/C++/ejemplo3.cpp
@@ -4,7 +4,6 @@ int main() {
     int x = 25;
     int *p = &x;
 
-    // Traducción literal manteniendo el uso de punteros y direcciones
     std::cout << "x = " << x << std::endl;
     std::cout << "&x = " << (void *)&x << std::endl;
     std::cout << "p = " << (void *)p << std::endl;
